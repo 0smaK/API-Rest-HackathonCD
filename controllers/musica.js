e6c45e4ccd4dca35800082c608ba3b595d938cc2
0smak <osmak@protonmail.com>
2019-03-19 21:20:25 +0100

    Hito 4 - [ Desarrollo ] - Terminado

--- controllers/musica.js
+++ controllers/musica.js
@@ -19,6 +19,97 @@ function getCancion(req, res) {
     })
 }
 
+function getCancionTitulo(req, res){
+    let songTitle = req.params.songTitle
+
+    Music.findOne({titulo: songTitle}, (err, cancion) => {
+        if(err) return res.status(500).send({
+            message:`Error al realizar la petición: ${err}`
+        })
+
+        if(!cancion) return res.status(404).send({
+            message: `No existe la canción ${songTitle}`
+        })
+
+        res.status(200).send({
+            cancion
+        })
+    })
+}
+
+function getCancionesArtista(req,res){
+    let songArtist = req.params.songArtist
+
+    Music.find({artista: songArtist}, (err,canciones) => {
+        if(err) return res.status(500).send({
+            message:`Error al realizar la petición ${err}`
+        })
+
+        if(!canciones) return res.status(404).send({
+            message: `No existe ninguna coincidencia con el artista ${songArtist}`
+        })
+
+        res.status(200).send({
+            canciones
+        })
+    })
+
+}
+
+function getCancionesAlbum(req,res){
+    let songAlbum = req.params.songAlbum
+
+    Music.find({album:songAlbum}, (err, canciones) => {
+        if(err) return res.status(500).send({
+            message: `Error al realizar la peticion ${err}`
+        })
+
+        if(!canciones) return res.status(404).send({
+            message: `No se ha encontrado ninguna coincidencia con el album ${songAlbum}`
+        })
+
+        res.status(200).send({
+            canciones
+        })
+    })
+}
+
+function getCancionesGenero(req,res){
+    let songGenre = req.params.songGenre
+
+    Music.find({genero: songGenre}, (err,canciones) => {
+        if(err) return res.status(500).send({
+            message: `Error al realizar la petición ${err}`
+        })
+
+        if(!canciones) return res.status(404).send({
+            message: `No se ha encontrado ninguna coincidencia con el genero ${songGenre}`
+        })
+
+        res.status(200).send({
+            canciones
+        })
+    })
+}
+
+function getCancionesYear(req,res){
+    let songYear = req.params.songYear
+
+    Music.find({year: songYear}, (err, canciones) => {
+        if(err) return res.status(500).send({
+            message: `Error al realizar la petición ${err}`
+        })
+
+        if(!canciones) return res.status(404).send({
+            message: `No se ha encontrado ninguna coincidencia con el año ${songYear}`
+        })
+
+        res.status(200).send({
+            canciones
+        })
+    })
+}
+
 function getCanciones(req, res) {
     Music.find({}, (err, canciones) => {
         if (err) return res.status(500).send({
@@ -60,7 +151,7 @@ function updateSong(req, res) {
     let songID = req.params.songId
     let update = req.body
 
-    Music.findOnersAndUpdate(songID, update, (err, songUpdated) => {
+    Music.findOneAndUpdate(songID, update, (err, songUpdated) => {
         if (err) res.status(500).send({
             message: `Error al actualizar la canción: ${err}`
         })
@@ -93,6 +184,11 @@ function deleteSong(req, res) {
 
 module.exports = {
     getCancion,
+    getCancionTitulo,
+    getCancionesArtista,
+    getCancionesGenero,
+    getCancionesYear,
+    getCancionesAlbum,    
     getCanciones,
     addSong,
     updateSong,
